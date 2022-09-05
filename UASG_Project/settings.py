ce4961897eb74f97f8e4615f0b049f2acf24c0e1
Shweta Patil <shwetap@gist.cdacitpark.in>
2022-09-05 17:09:53 +0530

    Registration page created

--- UASG_Project/settings.py
+++ UASG_Project/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+ENCRYPT_KEY = b'tmzHcYuvLUhxjcxZ4k_iqfCx-HUq6PCvdbXr4vOC5B4='
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,11 +79,19 @@ WSGI_APPLICATION = 'UASG_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'uasg_db',
+        'USER': 'root',
+        'PASSWORD': 'manager',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4'
+        }
+    },
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
